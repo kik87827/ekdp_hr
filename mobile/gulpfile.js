b8abeae4b07ab7a7e150485d8a18926e0a58a919
kik87hp <kik87827@gmail.com>
2025-06-07 03:07:33 +0900

    1차 세팅 완료료

--- mobile/gulpfile.js
+++ mobile/gulpfile.js
@@ -9,54 +9,54 @@ var gulp = require("gulp"),
 	beautify = require('gulp-beautify'),
 	htmlbeautify = require('gulp-html-beautify');
 
-gulp.task('default', ['scss','fileinclude','beautify', 'watch']);
+gulp.task('default', ['fileinclude', 'beautify', 'watch']);
 
 gulp.task('scss', function () {
 	gulp.src('./src/scss/*.scss')
-	.pipe(sourcemaps.init())
-	.pipe(scss().on('error', scss.logError))
-	.pipe(autoprefixer())
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('./dist/css/'))
+		.pipe(sourcemaps.init())
+		.pipe(scss().on('error', scss.logError))
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/css/'))
 });
 
 gulp.task('htmlbeautify', function () {
-	var options = { indent_with_tabs : true }
+	var options = { indent_with_tabs: true }
 	gulp.src('./src/**.html')
-	.pipe(htmlbeautify(options))
-	.pipe(gulp.dest('./dist/'))
+		.pipe(htmlbeautify(options))
+		.pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('jshint',function(){
+gulp.task('jshint', function () {
 	gulp.src('./src/js/front.js')
-	.pipe(jshint())
-	.pipe(jshint.reporter('default'))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
 });
 
-gulp.task('beautify',function(){
+gulp.task('beautify', function () {
 	gulp.src('./src/js/*.js')
-	.pipe(beautify.js({indent_size: 2}))
-	.pipe(gulp.dest('./dist/js/'))
+		.pipe(beautify.js({ indent_size: 2 }))
+		.pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('fileinclude',function(){
-	gulp.src(['./src/**.html'],{base : "./src/"})
-	.pipe(fileinclude({
-		prefix : '@@',
-		basepath : '@file'
-	}).on('error', function(){ console.log('path error')}))
-	// .pipe(removeEmptyLines())
-	.pipe(htmlbeautify({indent_with_tabs : true}))
-	.pipe(gulp.dest('./dist/'))
+gulp.task('fileinclude', function () {
+	gulp.src(['./src/**.html'], { base: "./src/" })
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file'
+		}).on('error', function () { console.log('path error') }))
+		// .pipe(removeEmptyLines())
+		.pipe(htmlbeautify({ indent_with_tabs: true }))
+		.pipe(gulp.dest('./dist/'))
 });
 
-gulp.task('webserver',function(){
+gulp.task('webserver', function () {
 	gulp.src('./dist/')
-	.pipe(webserver({
-		livereload : true,
-		open : true,
-		port : 7474
-	}));
+		.pipe(webserver({
+			livereload: true,
+			open: true,
+			port: 7474
+		}));
 });
 
 gulp.task('watch', function () {
